831b79ed3fe049c7dcab1a45066f91e0f23014e0
Shukri Yusof <shukriyusof65@gmail.com>
2021-05-11 09:37:38 +0800

    remove p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,4 +44,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.esm.min.js"></script>
-p
+
